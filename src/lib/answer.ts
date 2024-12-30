fc78692c305466c22a5a83fb6d91ca0c4c081e97
Joseff <joseffnic@gmail.com>
2024-12-30 19:26:36 +0000

    Add Serbian

--- src/lib/answer.ts
+++ src/lib/answer.ts
@@ -1,50 +1,56 @@
-import { toHiragana, toRomaji } from 'wanakana';
-import { game_config } from '$lib/stores/game-config';
-import { greek, cyrillic, exceptional_answers } from '$/lib/glyph-database';
-import { get } from 'svelte/store';
+import { toHiragana, toRomaji } from "wanakana";
+import { game_config } from "$lib/stores/game-config";
+import { greek, cyrillic, exceptional_answers } from "$/lib/glyph-database";
+import { get } from "svelte/store";
 
 export function getAnswers(glyph: string): string[] {
-	switch (get(game_config)?.path[0]) {
-		case 'kana':
-			return exceptional_answers.get(toHiragana(glyph)) ?? [toRomaji(glyph)];
-		case 'greek':
-			switch (get(game_config).path[1]) {
-				case 'transcriptions':
-					return greek.transcriptions[glyph.toLowerCase() as keyof typeof greek.transcriptions];
-				case 'names':
-					return greek.names[glyph.toLowerCase() as keyof typeof greek.names];
-				default:
-					return [];
-			}
-		case 'cyrillic':
-			switch (get(game_config).path[1]) {
-				case 'bulgarian':
-					return cyrillic.bg_transcriptions[
-						glyph.toLowerCase() as keyof typeof cyrillic.bg_transcriptions
-					];
-				case 'russian':
-					return cyrillic.ru_transcriptions[
-						glyph.toLowerCase() as keyof typeof cyrillic.ru_transcriptions
-					];
-				default:
-					return [];
-			}
-		default:
-			return [];
-	}
+  switch (get(game_config)?.path[0]) {
+    case "kana":
+      return exceptional_answers.get(toHiragana(glyph)) ?? [toRomaji(glyph)];
+    case "greek":
+      switch (get(game_config).path[1]) {
+        case "transcriptions":
+          return greek.transcriptions[
+            glyph.toLowerCase() as keyof typeof greek.transcriptions
+          ];
+        case "names":
+          return greek.names[glyph.toLowerCase() as keyof typeof greek.names];
+        default:
+          return [];
+      }
+    case "cyrillic":
+      switch (get(game_config).path[1]) {
+        case "bulgarian":
+          return cyrillic.bg_transcriptions[
+            glyph.toLowerCase() as keyof typeof cyrillic.bg_transcriptions
+          ];
+        case "russian":
+          return cyrillic.ru_transcriptions[
+            glyph.toLowerCase() as keyof typeof cyrillic.ru_transcriptions
+          ];
+        case "serbian":
+          return cyrillic.sr_transcriptions[
+            glyph.toLowerCase() as keyof typeof cyrillic.sr_transcriptions
+          ];
+        default:
+          return [];
+      }
+    default:
+      return [];
+  }
 }
 
 // To handle silent letters
 export function isSilent(glyph: string): boolean {
-	return getAnswers(glyph)[0] === '-';
+  return getAnswers(glyph)[0] === "-";
 }
 
 export function isCorrectAnswer(guess: string, actual: string): boolean {
-	if (!get(game_config)?.is_valid) return false;
-	switch (get(game_config).path[0]) {
-		case 'kana':
-			return getAnswers(actual).includes(toRomaji(guess.toLowerCase()));
-		default:
-			return getAnswers(actual).includes(guess);
-	}
+  if (!get(game_config)?.is_valid) return false;
+  switch (get(game_config).path[0]) {
+    case "kana":
+      return getAnswers(actual).includes(toRomaji(guess.toLowerCase()));
+    default:
+      return getAnswers(actual).includes(guess);
+  }
 }
--- src/lib/stores/game-config.ts
+++ src/lib/stores/game-config.ts
@@ -1,41 +1,44 @@
-import { sessionPersistentStore } from '$/lib/persistent-stores';
-import { full_keys } from '$/lib/glyph-configurations';
+import { sessionPersistentStore } from "$/lib/persistent-stores";
+import { full_keys } from "$/lib/glyph-configurations";
 
 interface Configuration {
-	id: string;
-	path: string[];
-	is_valid: boolean;
+  id: string;
+  path: string[];
+  is_valid: boolean;
 }
 
 function generatePathArray(id: string) {
-	return id.split('-');
+  return id.split("-");
 }
 
-export const game_config = sessionPersistentStore<Configuration>('game-config', {
-	id: 'hiragana-monographs',
-	path: generatePathArray('hiragana-monographs'),
-	is_valid: true
-});
-
-import { get } from 'svelte/store';
+export const game_config = sessionPersistentStore<Configuration>(
+  "game-config",
+  {
+    id: "hiragana-monographs",
+    path: generatePathArray("hiragana-monographs"),
+    is_valid: true,
+  }
+);
 
 export function updateGameConfig(new_id: string) {
-	game_config.update(() => {
-		return {
-			id: new_id,
-			path: generatePathArray(new_id),
-			is_valid: full_keys.includes(new_id)
-		};
-	});
+  game_config.update(() => {
+    return {
+      id: new_id,
+      path: generatePathArray(new_id),
+      is_valid: full_keys.includes(new_id),
+    };
+  });
 }
 
 export function updateGameConfigAtDepth(new_id: string, depth: number) {
-	game_config.update((config) => {
-		const updated_id = [...config.id.split('-').slice(0, depth), new_id].join('-');
-		return {
-			id: updated_id,
-			path: generatePathArray(updated_id),
-			is_valid: full_keys.includes(updated_id)
-		};
-	});
+  game_config.update((config) => {
+    const updated_id = [...config.id.split("-").slice(0, depth), new_id].join(
+      "-"
+    );
+    return {
+      id: updated_id,
+      path: generatePathArray(updated_id),
+      is_valid: full_keys.includes(updated_id),
+    };
+  });
 }
